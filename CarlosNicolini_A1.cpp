8b5fa35586e191e05479f455367f0f27b4b0dcde
CarlosNico <132309481+CarlosNico@users.noreply.github.com>
2023-07-19 22:57:59 -0700

    Add files via upload
    Lenguaje del código de la actividad 1 de la meteria de Lenguajes de Programación 1

---
+++ CarlosNicolini_A1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+ int main(){
+  
+int edad;
+  
+std::cout << "Digita tu edad:";
+std::cin >> edad;
+  
+ 
+     if (edad >=18 && edad <=65){
+         std::cout << "Eres mayor de edad y te estas tardando en trabajar." << std::endl;
+     } 
+     else if
+             (edad >65){
+         std::cout << "Eres muy mayor, debes estar jubilado." << std::endl;
+     }else {
+             std::cout << "Eres menor de edad, mejor termina tus estudios." << std::endl;
+     }
+     return 0;
+}
